7af3a8d62b12b4cedefa6955b595ec2d16428c0d
Hieu <hieu.n2395@gmail.com>
2020-12-01 16:26:51 +0700

    delete all

--- src/app/main/role/role.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { RoleComponent } from './role.component';
-export const roleRoutes: Routes = [
-    // 4200/main/user
-    { path: '', redirectTo: 'index', pathMatch: 'full' },
-    // 4200/main/home/index
-    { path: 'index', component: RoleComponent }
-];
